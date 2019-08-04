f97bf3dab024a46bf028e31e5a45b793be1fe30c
Fatema Sherif <fatemakhalid96@gmail.com>
2019-08-05 00:53:23 +0200

    Authentication configuration fixed

--- src/server/database/index.js
+++ src/server/database/index.js
@@ -1,5 +1,5 @@
 const knex = require('knex');
-const knexDb = knex({ client: 'pg',  connection: process.env.POSTGRES_DATABASE_CONNECTION});
+const knexDb = knex({ client: 'pg',  connection: 'http://postgres:pass@127.0.0.1:5432/royalmanta'});
 const bookshelf = require('bookshelf');
 const securePassword = require('bookshelf-secure-password');
 const db = bookshelf(knexDb);
